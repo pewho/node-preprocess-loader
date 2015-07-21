fd446e3e50a7b2cd94d74a5adb083809b1fb250d
pewho <pewhoo@gmail.com>
2015-07-21 11:27:19 +0200

    fix packageJson

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var pp = require('preprocess');
 
 module.exports = function(source) {
-	this.cacheable && this.cacheable();
 	var preprocessed_source = pp.preprocess(source);
 	return preprocessed_source;
 };
