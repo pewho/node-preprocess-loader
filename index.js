158ca6e4222f77095302e8a1f2ae73af7a7d2ad9
pewho <pewhoo@gmail.com>
2015-07-21 11:37:00 +0200

    Fix type to js preprocess

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var pp = require('preprocess');
 
 module.exports = function(source) {
-	var preprocessed_source = pp.preprocess(source);
+	var preprocessed_source = pp.preprocess(source, {}, {type: 'js'});
 	return preprocessed_source;
 };
